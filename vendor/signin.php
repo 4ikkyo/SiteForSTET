37f1f63e9ef278fb37aaab987c285bcdd9b9173f
Денис Петров <fresh2017ick@gmail.com>
2021-06-11 19:47:13 +0300

    registr

---
+++ vendor/signin.php
@@ -0,0 +1,36 @@
+<?php
+
+    session_start();
+    require_once 'connect.php';
+	
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+	
+	$query = "SELECT * FROM users WHERE `login` = '$login'";
+	$result = $connect->query($query);
+	$row = $result->fetch_assoc();
+	
+	if (password_verify($password, $row['password'])) {
+
+        $_SESSION['user'] = [
+            "id" => $row['id'],
+            "full_name" => $row['full_name'],
+            "avatar" => $row['avatar'],
+            "email" => $row['email']
+        ];
+
+        header('Location: ../profile.php');
+
+    } else {
+        $_SESSION['message'] = 'Не верный логин или пароль';
+        header('Location: ../index.php');
+    }
+
+    ?>
+
+<pre>
+    <?php
+    print_r($check_user);
+    print_r($row);
+    ?>
+</pre>
